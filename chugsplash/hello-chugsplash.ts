3df98edc51bbb0e80e7ec2c5a9f6b7fbc20c5be2
Kelvin Fichter <kelvinfichter@gmail.com>
2023-02-28 13:31:28 -0700

    feat: change number

--- chugsplash/hello-chugsplash.ts
+++ chugsplash/hello-chugsplash.ts
@@ -8,7 +8,7 @@ const config: UserChugSplashConfig = {
     MyFirstContract: {
       contract: 'HelloChugSplash',
       variables: {
-        number: 5,
+        number: 6,
         stored: true,
         storageName: 'First',
         otherStorage: '0x1111111111111111111111111111111111111111',
